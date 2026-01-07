8a8da5e8be92914dfcc00e48c02f8a771490fda4
Max Hill <maxhill0713@gmail.com>
2026-01-07 15:05:42 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,41 +1,39 @@
+import Challenges from './pages/Challenges';
 import Groups from './pages/Groups';
 import GymCommunity from './pages/GymCommunity';
 import GymSignup from './pages/GymSignup';
 import Gyms from './pages/Gyms';
-import Home from './pages/Home';
 import Leaderboard from './pages/Leaderboard';
 import Members from './pages/Members';
 import Messages from './pages/Messages';
 import Notifications from './pages/Notifications';
 import Plus from './pages/Plus';
+import Premium from './pages/Premium';
 import Profile from './pages/Profile';
 import Routines from './pages/Routines';
 import Search from './pages/Search';
-import Premium from './pages/Premium';
-import Challenges from './pages/Challenges';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
+    "Challenges": Challenges,
     "Groups": Groups,
     "GymCommunity": GymCommunity,
     "GymSignup": GymSignup,
     "Gyms": Gyms,
-    "Home": Home,
     "Leaderboard": Leaderboard,
     "Members": Members,
     "Messages": Messages,
     "Notifications": Notifications,
     "Plus": Plus,
+    "Premium": Premium,
     "Profile": Profile,
     "Routines": Routines,
     "Search": Search,
-    "Premium": Premium,
-    "Challenges": Challenges,
 }
 
 export const pagesConfig = {
-    mainPage: "Home",
+    mainPage: "Challenges",
     Pages: PAGES,
     Layout: __Layout,
 };
