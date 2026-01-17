7a3389be7c51f55a6c6fb28253d3856958612f5d
Max Hill <maxhill0713@gmail.com>
2026-01-17 14:04:04 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,6 +1,4 @@
-import Groups from './pages/Groups';
 import GymCommunity from './pages/GymCommunity';
-import GymOwnerDashboard from './pages/GymOwnerDashboard';
 import GymSignup from './pages/GymSignup';
 import Gyms from './pages/Gyms';
 import Home from './pages/Home';
@@ -16,13 +14,12 @@ import Profile from './pages/Profile';
 import Routines from './pages/Routines';
 import Search from './pages/Search';
 import UserProfile from './pages/UserProfile';
+import GymOwnerDashboard from './pages/GymOwnerDashboard';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
-    "Groups": Groups,
     "GymCommunity": GymCommunity,
-    "GymOwnerDashboard": GymOwnerDashboard,
     "GymSignup": GymSignup,
     "Gyms": Gyms,
     "Home": Home,
@@ -38,10 +35,11 @@ export const PAGES = {
     "Routines": Routines,
     "Search": Search,
     "UserProfile": UserProfile,
+    "GymOwnerDashboard": GymOwnerDashboard,
 }
 
 export const pagesConfig = {
-    mainPage: "Groups",
+    mainPage: "GymCommunity",
     Pages: PAGES,
     Layout: __Layout,
 };
