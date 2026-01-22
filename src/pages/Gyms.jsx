4b5bdd8982b1d0434c841f3aa811c161ea7c0b61
Max Hill <maxhill0713@gmail.com>
2026-01-22 16:56:58 +0000

    File changes

--- src/pages/Gyms.jsx
+++ src/pages/Gyms.jsx
@@ -358,6 +358,7 @@ export default function Gyms() {
               <JoinWithCodeModal
               open={showJoinWithCode}
               onClose={() => setShowJoinWithCode(false)}
+              currentUser={currentUser}
               />
               </div>
               );
